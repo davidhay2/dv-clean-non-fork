8438914540da5c1eb82cbd8528b0679b6cf93c1e
David Hay <david.j.hay@gmail.com>
2022-03-24 17:02:01 +0000

    added tests for css and js

--- datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/StaticAssetsTest.java
+++ datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/StaticAssetsTest.java
@@ -2,17 +2,26 @@ package org.datavaultplatform.webapp.app;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AddTestProperties
 public class StaticAssetsTest {
+
+  private static final MediaType JAVASCRIPT = MediaType.parseMediaType("application/javascript");
+  private static final MediaType CSS = MediaType.parseMediaType("text/css");
 
   @Autowired
   MockMvc mvc;
@@ -20,8 +29,24 @@ public class StaticAssetsTest {
   @Test
   void testStaticJavascript() throws Exception {
 
-    mvc.perform(get("/js/hellowWorld.js")).andDo(print());
+    MvcResult res = mvc.perform(get("/js/helloWorld.js"))
+        .andExpect(content().string(Matchers.containsString("Hello World!")))
+        .andDo(print())
+        .andReturn();
 
+    MediaType actual = MediaType.parseMediaType(res.getResponse().getContentType());
+    Assertions.assertTrue(actual.isCompatibleWith(JAVASCRIPT));
   }
 
+  @Test
+  void testStaticCSS() throws Exception {
+
+    MvcResult res = mvc.perform(get("/css/main.css"))
+        .andExpect(content().string(Matchers.containsString("hello-title")))
+        .andDo(print())
+        .andReturn();
+
+    MediaType actual = MediaType.parseMediaType(res.getResponse().getContentType());
+    Assertions.assertTrue(actual.isCompatibleWith(CSS));
+  }
 }
